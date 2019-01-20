481bdc8a70132bf1e1a55213090ec53af68aa1b2
blackphoenix-310 <phoenix.rebirth.310@gmail.com>
2019-01-20 20:29:58 +0000

    Update guest_install.sh
    add case statement and ubuntu_installation

--- bash/guest_install.sh
+++ bash/guest_install.sh
@@ -24,7 +24,7 @@ virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
 --cdrom=/opt/kubuntu-18.04.1-desktop-amd64.iso \
 --os-type linux \
 --os-variant ubuntu16.04 \
---network network=gateway \
+--network default \
 --graphics spice,listen='172.16.30.125' \
 --noautoconsole &
 }
@@ -37,10 +37,24 @@ virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
 --cdrom=/opt/openSUSE-Tumbleweed-DVD-x86_64-Snapshot20190108-Media.iso \
 --os-type linux \
 --os-variant opensusetumbleweed \
---network network=gateway \
+--network default \
 --graphics spice,listen='172.16.30.125' \
 --noautoconsole &
 }
+
+ubuntu_bionic () {
+vol_info
+install_info
+virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
+--disk path=$STORE \
+--cdrom=/mnt/iso/kubuntu-18.04.1-desktop-amd64.iso \
+--os-type linux \
+--os-variant ubuntu16.04 \
+--network default \
+--graphics spice,listen='172.16.30.125' \
+--noautoconsole &
+}
+
 
 centos_7 () {
 vol_info
@@ -50,7 +64,7 @@ virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
 --cdrom=CentOS-7-x86_64-DVD-1810.iso \
 --os-type linux \
 --os-variant centos7.0 \
---network network=gateway \
+--network default \
 --graphics spice,listen='172.16.30.125' \
 --noautoconsole &
 }
@@ -63,7 +77,7 @@ virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
 --cdrom=/opt/ubuntu-18.04.1-live-server-amd64.iso \
 --os-type linux \
 --os-variant ubuntu18.04 \
---network network=gateway \
+--network default \
 --graphics spice,listen='172.16.30.125' \
 --noautoconsole &
 }
@@ -76,7 +90,7 @@ virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
 --cdrom=/opt/debian-9.6.0-amd64-DVD-1.iso \
 --os-type linux \
 --os-variant debian9 \
---network network=gateway \
+--network default \
 --graphics spice,listen='172.16.30.125' \
 --noautoconsole &
 }
@@ -89,7 +103,7 @@ virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
 --cdrom=/opt/<window10_iso> \
 --os-type windows \
 --os-variant win10 \
---network network=gateway \
+--network default \
 --graphics spice,listen='172.16.30.125' \
 --noautoconsole &
 }
@@ -102,7 +116,7 @@ virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
 --cdrom=/opt/<windows2k12_iso> \
 --os-type windows \
 --os-variant w2k12 \
---network network=gateway \
+--network default \
 --graphics spice,listen='172.16.30.125' \
 --noautoconsole &
 }
@@ -115,7 +129,42 @@ virt-install --name $VM_NAME --vcpu $CPU --ram $MEM \
 --cdrom=/opt/<windows2k16_iso> \
 --os-type windows \
 --os-variant w2k16 \
---network network=gateway \
+--network default \
 --graphics spice,listen='172.16.30.125' \
 --noautoconsole &
 }
+
+echo "Select an OS to install."
+echo "<1> Kubuntu, <2> Ubuntu 18.04 Bionic, <3> OpenSUSE Tumbleweed"
+echo "<4> CentOS 7, <5> Ubuntu Server Bionic <6> Debian Stretch"
+echo "<7> Windows 10, <8> Windows Server 2012(R2), <9> Windows Server 2016"
+
+read -p "Choose an OS to install. " CHOICE
+case "$CHOICE" in
+
+        1) kubuntu
+        ;;
+
+        2) ubuntu_bionic
+        ;;
+
+        3) suse-tumble
+        ;;
+
+        4) centos_7
+        ;;
+
+        5) ubuntu_server_bionic
+        ;;
+
+        6) deb9
+        ;;
+
+        7) win_10
+        ;;
+
+        8) win_2k12
+        ;;
+
+        9) serv_2k16
+        ;;
