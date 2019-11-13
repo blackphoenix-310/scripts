57fea836c8cef4ae13c8c11e693a08195f8c7749
blackphoenix-310 <phoenix.rebirth.310@gmail.com>
2019-11-13 19:43:59 +0000

    adding backup script to virt_script.sh

--- bash/virt_script.sh
+++ bash/virt_script.sh
@@ -137,6 +137,30 @@ then
 fi
 }
 
+backup_guest () {
+# declare/initialize global array
+guests=($(virsh -c qemu:///system list | awk '/running/{print $2}'))
+
+# global variables
+SNAP_DEST=/mnt/snap/
+CPY_DEST=/mnt/backup/
+
+for n in "${guests[@]}"
+do
+   # initialize block storage array list and sets array variable
+   blks=($(virsh -c qemu:///system domblklist --domain ${n} | awk '/vda/{print $2}'))
+   BLK_CALL=$(echo ${blks[0]})
+   # create external snapshot
+   virsh -c qemu:///system snapshot-create-as --domain ${n} --name ${n} --diskspec vda,file=${SNAP_DEST}${n}.qcow2 \
+   --disk-only --atomic --no-metadata --quiesce | logger -t guest_snap
+   # syncs with backup destination
+   rsync -azq ${BLK_CALL} ${CPY_DEST}${n} | logger -t guest_sync
+   # active blockcommit
+   virsh -c qemu:///system blockcommit --domain ${n} vda --active --pivot --verbose | logger -t guest_pivot
+   rm -rf ${SNAP_DEST}${n}.qcow2
+done
+}
+
 echo "============================"
 echo "KVM/QEMU Virtual Management"
 echo "============================"
@@ -153,32 +177,35 @@ echo "3). List available volumes in host pools."
 echo "4). List available networks."
 echo "5). Install a KVM Guest."
 echo "6). Migrate a KVM Guest."
-echo
+echo "7). Backup a KVM Guest."
 read -p "Select an option: " OPT
 
 case "$OPT" in
-    
+
     1) list_guest
        ;;
-    
+
     2) list_pool
        ;;
-      
+
     3) list_vol
        ;;
-    
+
     4) list_net
        ;;
-       
+
     5) guest_install
        ;;
-    
+
     6) migrate_guest
        ;;
-       
+
+    7) backup_guest
+       ;;
+
     *)
       echo "Invalid option..."
       echo "Choose a valid option"
       ;;
-      
+
 esac
