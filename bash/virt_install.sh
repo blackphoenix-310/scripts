2e892c486adc0bcb16991d957c6e0b3763452aa9
blackphoenix-310 <phoenix.rebirth.310@gmail.com>
2020-01-01 04:30:52 +0000

    added virt_install.sh

--- bash/virt_install.sh
+++ bash/virt_install.sh
@@ -3,18 +3,16 @@
 NAME=$1
 STORE_SIZE=$2
 STORE=$3
-POOL_NAME=$4
-ISO_PATH=$5
 
 if [ ! -f /mnt/guest_store/$NAME ]; then
-  virsh vol-create-as --pool ${POOL_NAME} --name ${NAME} --capacity ${STORE_SIZE}G --format qcow2
+  virsh vol-create-as --pool guest_store --name ${NAME} --capacity ${STORE_SIZE}G --format qcow2
   virt-install \
  --name $NAME \
  --memory 2048 \
  --vcpus 2 \
  --boot hd,cdrom,network,uefi,menu=on \
  --disk path=/mnt/guest_store/${NAME} \
- --cdrom $ISO_PATH \
+ --cdrom /mnt/iso/CentOS-8-x86_64-1905-dvd1.iso \
  --network bridge=br0 \
  --graphics spice,listen='172.16.30.123' \
  --os-type linux \
